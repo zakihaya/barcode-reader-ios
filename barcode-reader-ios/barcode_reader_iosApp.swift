c4f4bc51ca28bbfdcb6c8dedf32c28d4965f8b9e
hayazaki <toru.hayazaki@gmail.com>
2022-04-04 14:13:34 +0900

    Initial Commit

---
+++ barcode-reader-ios/barcode_reader_iosApp.swift
@@ -0,0 +1,17 @@
+//
+//  barcode_reader_iosApp.swift
+//  barcode-reader-ios
+//
+//  Created by haayzaki on 2022/04/04.
+//
+
+import SwiftUI
+
+@main
+struct barcode_reader_iosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
